7652ab7d44d4c57ff368396193b11728f6ecd205
isnowify <isnowify@outlook.com>
2020-03-24 14:14:15 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from googletrans import Translator
+import json
+from time import sleep
+translator = Translator(service_urls=[
+    'translate.google.cn',
+])
+a= input('Type the original text here...')
+count = 0
+num = 1
+dict = {'ja', 'ko', 'ms','es','th','uk','vi','zh-CN','zh-TW','en','hi','id','ms','mn','my','ne','bg','ar','hy'}
+stop=input('你想啥时候结束哦：')
+for key in dict:
+    b = translator.translate(a,dest=key)
+    count = 1
+
+    if count == 1:
+        if num == stop:
+            break
+        else:
+            for i in range(1,int(stop)):
+                sleep(1)
+                b = translator.translate(b.text,dest=key)
+                num = num + 1
+
+final = translator.translate(b.text,dest='zh-CN')
+print(final.text)
